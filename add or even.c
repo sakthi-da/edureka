0251d850237ddc8b97d4d06ba3a529662d85fa90
sakthi-da <113973018+sakthi-da@users.noreply.github.com>
2022-11-26 12:46:42 +0530

    Add files via upload

---
+++ add or even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+void main
+{
+ int num;
+ printf("/n Enter a number");
+ scanf ("%d";&num);
+ for(int=0;i<num;i++)
+   if(num/2==0)
+    {
+     printf("%d",&num);
+    }
+}
